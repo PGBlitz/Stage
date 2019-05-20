e7596927624e99222c20859b1de92e3fc677272c
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-05-20 18:10:10 -0400

    update

--- versions.sh
+++ versions.sh
@@ -5,13 +5,13 @@
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
-path78="/pg/stage/functions"
+# Changing the Numbers Higher will result in force updating that section
+################################################################################
 
 # key parts
-source "${path78}/core.sh"
+source "/pg/stage/functions/core.sh"
 
 versioninfo () {
-
 # Set variable numbers, plus number up to force update
 pgstore "install.merger" "1"
 pgstore "install.python" "1"
@@ -28,7 +28,6 @@ pgstore "install.alias" "1"
 pgstore "install.cleaner" "1"
 pgstore "install.gcloud" "1"
 pgstore "install.hetzner" "1"
-pgstore "install.amazonaws" "1"
 pgstore "install.watchtower" "1"
 pgstore "install.installer" "1"
 pgstore "install.prune" "1"
