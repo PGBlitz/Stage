14668b3485b79ea547c4a927ddf9e3674e512a88
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-05-24 15:50:56 -0400

    update

--- functions/core.sh
+++ functions/core.sh
@@ -5,37 +5,10 @@
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
-pgstore () {
-  maindir="/pg/var"
-  if [[ ! -e "${maindir}/${1}" ]]; then echo "${2}" > "${maindir}/${1}"; fi
-  echo "${2}" > "${maindir}/${1}"
-}
+serverid () {
 
-pgrecall () {
-  maindir="/pg/var"
-  if [[ ! -e "${maindir}/${1}" ]]; then touch "${maindir}/${1}"; fi
-  recallvar=$(cat "${maindir}/${1}")
-}
+# Generates a Random Server ID until user changes via options/pgvault
 
-variable () {
-  file="$1"
-  if [ ! -e "$file" ]; then echo "$2" > $1; fi
-}
 
-vartouch () {
-  file="$1"
-  if [ ! -e "$file" ]; then touch "$1"; fi
-}
 
-core () {
-    path95="/pg/var/install"
-    mkdir -p "${path95}"
-    vartouch "${path95}/${1}.stored"
-    start=$(cat "${path95}/${1}" 1>/dev/null 2>&1)
-    stored=$(cat "${path95}/${1}".stored 1>/dev/null 2>&1)
-    #if [ "$start" != "$stored" ]; then
-    if [ "0" != "1" ]; then #this is temp
-      $1
-      echo "${path95}/${1}" > "${path95}.${1}.stored";
-    fi
 }
