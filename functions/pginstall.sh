c29d589b2131f5d15ad04c0892722110fd5d2f4f
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-06-03 08:19:56 -0400

    update

--- functions/pginstall.sh
+++ functions/pginstall.sh
@@ -18,26 +18,19 @@ pginstall () {
   # - good - core mergerinstall
   # - ok (need to skip those ignore containers - core dockerinstall)
   # - good - core dockerassist
-  echo "FLAG1"
   # Roles Ensure that PG Replicates and has once if missing; important for startup, cron and etc
   pgapps
 
   # Complete Rest of the Process
   # portainer
   # pgui
-  echo "FLAG2"
   core motd &>/dev/null &
   core hetzner &>/dev/null &
   # [good] core gcloud
   # core watchtower
-  echo "FLAG3"
   core prune
-  echo "FLAG4"
   pgedition
-  echo "FLAG5"
-  echo "FLAG6"
   emergency
-  echo "FLAG7"
   pguinotice
   pgdeploy
 }
