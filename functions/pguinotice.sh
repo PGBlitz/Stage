1f33d8081b6e01a1cddf9c788e3a23bc66d04a4d
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-06-03 08:19:11 -0400

    update

--- functions/pguinotice.sh
+++ functions/pguinotice.sh
@@ -7,23 +7,29 @@
 ################################################################################
 pguinotice () {
 
-if
+# note that this is only a one time notice for new installs
+if [[ ! -e "/pg/var/pgui.notice" ]]; then
 
 tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ↘️  PG User Interface (PGUI) Installed
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
 INFORMATION:  PGUI is a simple interface that provides information,
 warnings, and stats that will assist both yourself and tech support!
-To turn this off, goto settings and turn off/on the PG User Interface!
+
+WARNING:
+If not secure, anyone in the world can visit your interface! Protect it by
+deploy PGShield later on!
+
+TO TURN OFF:
+To turn this off, goto settings and turn it off/on the PG User Interface!
 
 VISIT:
 https://pgui.domain.com | http://pgui.domain.com:8555 | ipv4:8555
-
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 EOF
 read -p 'Acknowledge Info | Press [ENTER] ' typed < /dev/tty
-
+touch > /pg/var/pgui.notice
+fi
 }
