70325477ae48128700b73b9361980ca364da1073
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-06-03 08:32:16 -0400

    update

--- functions/pginstall.sh
+++ functions/pginstall.sh
@@ -9,25 +9,26 @@ pginstall () {
 # Runs through the install process order
   updateprime
   bash /pg/stage/functions/gcechecker.sh
-  # - good - core pythonstart
-  # - good - core aptupdate
+  core pythonstart
+  core aptupdate
   core alias_install &>/dev/null &
-  # - good - core folders
-  # - good - serverid
-  # - good - core dependency
-  # - good - core mergerinstall
-  # - ok (need to skip those ignore containers - core dockerinstall)
-  # - good - core dockerassist
+  core folders
+  serverid
+  core dependency
+  core mergerinstall
+  core dockerinstall)
+  core dockerassist
+
   # Roles Ensure that PG Replicates and has once if missing; important for startup, cron and etc
   pgapps
 
   # Complete Rest of the Process
-  # portainer
-  # pgui
+  portainer
+  pgui
   core motd &>/dev/null &
   core hetzner &>/dev/null &
-  # [good] core gcloud
-  # core watchtower
+  core gcloud
+  core watchtower
   core prune
   pgedition
   emergency
