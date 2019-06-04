d64559fb90dd96577eca796c0e2b43254756bbf2
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-06-04 08:59:02 -0400

    update

--- functions/core.sh
+++ functions/core.sh
@@ -6,13 +6,13 @@
 # GNU:        General Public License v3.0
 ################################################################################
 pgstore () {
-  maindir="/pg/var"
+  maindir="/pg/var/install"
   if [[ ! -e "${maindir}/${1}" ]]; then echo "${2}" > "${maindir}/${1}"; fi
   echo "${2}" > "${maindir}/${1}"
 }
 
 pgrecall () {
-  maindir="/pg/var"
+  maindir="/pg/var/install"
   if [[ ! -e "${maindir}/${1}" ]]; then touch "${maindir}/${1}"; fi
   recallvar=$(cat "${maindir}/${1}")
 }
