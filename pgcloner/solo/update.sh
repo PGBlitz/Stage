8c785563a7f789ac6dea8520cac3de989b651394
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-08-25 19:07:07 -0400

    update

--- pgcloner/solo/update.sh
+++ pgcloner/solo/update.sh
@@ -11,6 +11,6 @@ echo 'install' > /pg/var/pgcloner.rolename
 echo 'install' > /pg/var/pgcloner.roleproper
 echo 'Install' > /pg/var/pgcloner.projectname
 echo 'v90' > /pg/var/pgcloner.projectversion
-
+echo "FLAG1"
 ### START PROCESS
 ansible-playbook /pg/stage/pgcloner/core/primary.yml
