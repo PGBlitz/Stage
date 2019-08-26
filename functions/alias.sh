8aea09a7f8e8e11aa58baa7370063b8344ed6dd0
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-08-25 21:22:22 -0400

    update

---
+++ functions/alias.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+alias () {
+    ansible-playbook /pg/stage/ymls/alias.yml 
+}
