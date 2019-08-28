057de37d26c49977b7b4520412cf23134addf763
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-08-28 09:16:08 -0400

    update

--- service/permissions.sh
+++ service/permissions.sh
@@ -3,19 +3,19 @@ sleep 10
 
 while [[ "$admin90210" == "admin90210" ]]; do
 
-  if [[ -e == "/pg/nzbget" ]]; then
+  if [[ -e "/pg/nzbget" ]]; then
   chmod -R 775 /pg/nzbget
   chown -R 1000:1000 /pg/nzbget; fi
 
-  if [[ -e == "/pg/sabnzbd" ]]; then
+  if [[ -e "/pg/sabnzbd" ]]; then
   chmod -R 775 /pg/nzbget
   chown -R 1000:1000 /pg/nzbget; fi
 
-  if [[ -e == "/pg/rutorrent" ]]; then
+  if [[ -e "/pg/rutorrent" ]]; then
   chmod -R 775 /pg/rutorrent
   chown -R 1000:1000 /pg/rutorrent; fi
 
-  if [[ -e == "/pg/qbitorrent" ]]; then
+  if [[ -e "/pg/qbitorrent" ]]; then
   chmod -R 775 /pg/qbitorrent
   chown -R 1000:1000 /pg/qbitorrent; fi
 
