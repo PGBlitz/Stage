1377a461ed1a57b5bf818121edd07a9a6cf3006b
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-08-28 06:09:08 -0400

    update

---
+++ functions/permissions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+prune () {
+    ansible-playbook "/pg/functions/service/permissions.yml"
+}
---
+++ service/permissions.sh
@@ -0,0 +1,22 @@
+admin90210="admin90210"
+sleep 10
+
+while [[ "$admin90210" == "admin90210" ]]; do
+
+  if [[ -e == "/pg/nzbget" ]]; then
+  chmod -R 775 /pg/nzbget
+  chown -R 1000:1000 /pg/nzbget; fi
+
+  if [[ -e == "/pg/sabnzbd" ]]; then
+  chmod -R 775 /pg/nzbget
+  chown -R 1000:1000 /pg/nzbget; fi
+
+  if [[ -e == "/pg/rutorrent" ]]; then
+  chmod -R 775 /pg/rutorrent
+  chown -R 1000:1000 /pg/rutorrent; fi
+
+  if [[ -e == "/pg/qbitorrent" ]]; then
+  chmod -R 775 /pg/qbitorrent
+  chown -R 1000:1000 /pg/qbitorrent; fi
+
+done
