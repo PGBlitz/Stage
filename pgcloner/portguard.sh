dd15bdb2874c1c59234242143f34fb96b12e6ab6
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-08-28 20:11:39 -0400

    update

---
+++ pgcloner/portguard.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+
+### FILL OUT THIS AREA ###
+echo 'portguard' > /pg/var/pgcloner.rolename
+echo 'PortGuard' > /pg/var/pgcloner.roleproper
+echo 'PortGuard' > /pg/var/pgcloner.projectname
+echo 'v90' > /pg/var/pgcloner.projectversion
+echo 'portguard.sh' > /pg/var/pgcloner.startlink
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+echo "💬 PortGuard enables the user to open or close container ports" > /pg/var/pgcloner.info
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+### START PROCESS
+bash /pg/stage/pgcloner/core/main.sh
