f05319d98aa4d64ce70451c163b7430c48e1adbd
Cody <ChaosZero112@users.noreply.github.com>
2020-11-11 15:03:19 -0500

    Update mergerinstall.sh
    Bump mergerfs. Add focal.

--- functions/mergerinstall.sh
+++ functions/mergerinstall.sh
@@ -7,7 +7,7 @@
 ################################################################################
 mergerinstall () {
 
-  ub16check=$(cat /etc/*-release | grep xenial)
+  ub20check=$(cat /etc/*-release | grep focal)
   ub18check=$(cat /etc/*-release | grep bionic)
   deb9check=$(cat /etc/*-release | grep stretch)
   activated=false
@@ -16,24 +16,24 @@ mergerinstall () {
     apt-get remove mergerfs -y
     mkdir -p /pg/var
 
-    if [ "$ub16check" != "" ]; then
+    if [ "$ub20check" != "" ]; then
     activated=true
-    echo "ub16" > /pg/var/mergerfs.version
-    wget "https://github.com/trapexit/mergerfs/releases/download/2.25.1/mergerfs_2.25.1.ubuntu-xenial_amd64.deb"
+    echo "ub20" > /pg/var/mergerfs.version
+    wget "https://github.com/trapexit/mergerfs/releases/download/2.31.0/mergerfs_2.31.0.ubuntu-focal_amd64.deb"
 
     elif [ "$ub18check" != "" ]; then
       activated=true
       echo "ub18" > /pg/var/mergerfs.version
-      wget "https://github.com/trapexit/mergerfs/releases/download/2.25.1/mergerfs_2.25.1.ubuntu-bionic_amd64.deb"
+      wget "https://github.com/trapexit/mergerfs/releases/download/2.31.0/mergerfs_2.31.0.ubuntu-bionic_amd64.deb"
 
     elif [ "$deb9check" != "" ]; then
       activated=true
       echo "deb9" > /pg/var/mergerfs.version
-      wget "https://github.com/trapexit/mergerfs/releases/download/2.25.1/mergerfs_2.25.1.debian-stretch_amd64.deb"
+      wget "https://github.com/trapexit/mergerfs/releases/download/2.31.0/mergerfs_2.31.0.debian-stretch_amd64.deb"
 
     elif [ "$activated" != "true" ]; then
       activated=true && echo "ub18 - but didn't detect correctly" > /pg/var/mergerfs.version
-      wget "https://github.com/trapexit/mergerfs/releases/download/2.25.1/mergerfs_2.25.1.ubuntu-bionic_amd64.deb"
+      wget "https://github.com/trapexit/mergerfs/releases/download/2.31.0/mergerfs_2.31.0.ubuntu-bionic_amd64.deb"
     else
       apt-get install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y
       git clone https://github.com/trapexit/mergerfs.git
